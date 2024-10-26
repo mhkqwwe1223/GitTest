5a08167e04b853c84ae987695534d3636c5d6ad5
mhkqwwe1223 <mhkqwwe1223@naver.com>
2024-10-26 19:47:50 +0900

    커밋 테스트

---
+++ app/src/main/java/com/example/myhiiapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myhiiapplication
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.textHello).apply {
+            setOnClickListener {
+                text = "Hi, World!"
+            }
+        }
+    }
+}
